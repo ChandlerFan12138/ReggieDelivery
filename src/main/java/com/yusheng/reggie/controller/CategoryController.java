01626ef9169ebc4303587276b7ba2118c9a26c34
Yusheng <524592395@qq.com>
2022-08-14 23:48:14 +0100

    The category page finished

--- src/main/java/com/yusheng/reggie/controller/CategoryController.java
+++ src/main/java/com/yusheng/reggie/controller/CategoryController.java
@@ -1,15 +1,14 @@
 package com.yusheng.reggie.controller;
 
 
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.yusheng.reggie.common.R;
 import com.yusheng.reggie.entity.Category;
 import com.yusheng.reggie.service.CategoryService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @Slf4j
 @RestController
@@ -24,5 +23,20 @@ public class CategoryController {
         log.info("category:{}",category);
         categoryService.save(category);
         return R.success("New category add successfully");
+    }
+
+    @GetMapping("/page")
+    public R<Page> page(int page, int pageSize){
+        //分页构造器
+        Page<Category> pageInfo = new Page<>(page,pageSize);
+
+//      条件构造器
+        LambdaQueryWrapper<Category> queryWrapper = new LambdaQueryWrapper<>();
+        queryWrapper.orderByAsc(Category::getSort);
+
+//        进行分页查询
+        categoryService.page(pageInfo,queryWrapper);
+
+        return R.success(pageInfo);
     }
 }
